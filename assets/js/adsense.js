8d08cbae2f5b27c6acd87038259dd7641d99d698
Javier Ocampos <xavier.ocampos@gmail.com>
2026-03-19 18:37:41 -0600

    Update

--- assets/js/adsense.js
+++
@@ -1,8 +0,0 @@
-// assets/js/adsense.js
-(function() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4585804237331442";
-    script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
-})();
