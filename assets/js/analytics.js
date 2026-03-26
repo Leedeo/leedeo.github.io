c15d5c682e2ec5cd524ce867bdeac29f4d4e6fee
Javier Ocampos <xavier.ocampos@gmail.com>
2026-03-26 05:10:53 -0600

    Update

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -5,6 +5,13 @@ function gtag() { dataLayer.push(arguments); }
 function loadGA() {
     if (window._gaLoaded) return;
     window._gaLoaded = true;
+
+    // Preconnect dinámico para evitar advertencias de Lighthouse en usuarios sin consentimiento
+    var p = document.createElement('link');
+    p.rel = 'preconnect';
+    p.href = 'https://www.googletagmanager.com';
+    document.head.appendChild(p);
+
     var s = document.createElement('script');
     s.async = true;
     s.src = 'https://www.googletagmanager.com/gtag/js?id=G-2SB71QMPV3';
