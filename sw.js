4e6725404414b9849c018f9d9fda72481d2f3f93
Javier Ocampos <xavier.ocampos@gmail.com>
2026-04-08 02:53:02 -0600

    Update

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker para Leedeo Studio - Cacheo básico de assets estáticos
+// Versión: 1.0 - Compatible con Cloudflare
+
+const CACHE_NAME = 'leedeo-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/assets/css/tailwind.min.css',
+  '/assets/css/style.css',
+  '/assets/js/init.js',
+  '/favicon.ico',
+  '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
